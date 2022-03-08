b58f8bc627fa518b0b7336cb6236818cac4b6a95
repzlaw <ibitoyedavid@gmail.com>
2022-03-08 10:56:27 +0100

    disabled timestamps and added fillable properties for pokemon model

--- app/Models/Pokemon.php
+++ app/Models/Pokemon.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Pokemon extends Model
 {
     use HasFactory;
+    protected $table = 'pokemons';
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'identifier',
+        'species_id',
+        'height',
+        'weight',
+        'base_experience',
+        'order',
+        'is_default',
+    ];
 }
