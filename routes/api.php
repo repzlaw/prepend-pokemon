ada37f2f3f70dbe5285f77a2458516098c52315f
repzlaw <ibitoyedavid@gmail.com>
2022-03-08 10:25:30 +0100

    defined authentication routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//authentication routes for register and login
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/login', [AuthController::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
+
