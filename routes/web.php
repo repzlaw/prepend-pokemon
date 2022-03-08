2fb8583d973f0c90bb38c69c99274045b019ad78
repzlaw <ibitoyedavid@gmail.com>
2022-03-08 10:53:32 +0100

    added web path for my react pages

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{path?}', function () {
+    return view('app');
+});
+Route::get('/{path?}/{path2?}', function () {
+    return view('app');
+});
+Route::get('/{path?}/{path2?}/{path3?}', function () {
+    return view('app');
 });
